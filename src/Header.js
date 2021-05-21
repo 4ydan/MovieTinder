e6edb06248b3844e6538f6d6bd33e04c02fcf5f8
adi <ghazani.aydan@gmail.com>
2021-05-22 00:28:49 +0200

    Small padding changes

--- src/Header.js
+++ src/Header.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import "./Header.css";
 import IconButton from '@material-ui/core/IconButton';
 import WhatshotIcon from '@material-ui/icons/Whatshot';
-import HomeIcon from '@material-ui/icons/Home';
+import EmojiObjectsIcon from '@material-ui/icons/EmojiObjects';
 
 
 const Header = () => {
@@ -11,12 +11,12 @@ const Header = () => {
         <div className="header">
             <Link to="/">
             <IconButton>
-                <HomeIcon className="header_icon"  fontSize="large" />
+                <EmojiObjectsIcon className="header_icon"  fontSize="middle" />
             </IconButton>
             </Link>
             <Link to="trending">
             <IconButton>
-                <WhatshotIcon className="header_icon"  fontSize="large" />
+                <WhatshotIcon className="header_icon"  fontSize="middle" />
             </IconButton>
             </Link>
         </div>
